c4476280b70f6232f15800e4d399662e18c5267b
chisl <info@chisl.io>
2017-12-29 10:09:53 +0100

    Initial commit

---
+++ LC709203F.h
@@ -0,0 +1,9 @@
+/*
+ * name:        LC709203F
+ * description: Smart LiB Gauge Battery Fuel Gauge LSI For 1‐Cell Lithium‐ion/Polymer (Li+)
+ * manuf:       ON Semiconductor
+ * version:     0.1
+ * url:         http://www.onsemi.com/pub/Collateral/LC709203F-D.PDF
+ * date:        2017-12-29
+ */
+
